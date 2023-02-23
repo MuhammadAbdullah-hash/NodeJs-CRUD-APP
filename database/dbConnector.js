80048e37642030ae4b96f88d6340f8cdbda2a1c5
Muhammad Abdullah <abdullahquddus&@gmail.com>
2023-02-24 02:47:34 +0500

    new4

--- database/dbConnector.js
+++ database/dbConnector.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : process.env.CONNECTION_LIMIT,
   host: process.env.HOST,
-  user: process.env.USER,
+  user: "sql12600308",
   password : process.env.PASSWSORD,
   port  : process.env.DB_PORT,
   database : process.env.DB_NAME
